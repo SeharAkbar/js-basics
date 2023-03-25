89f16d48c5d4c5bc10d9e10e724b294fba616059
Sehar Akbar <seharakbar224@gmail.com>
2023-03-25 14:41:07 +0500

    add

---
+++ if-else.js
@@ -0,0 +1,17 @@
+const doesDrive =true;
+const  age =65;
+// if (age==19) {
+//     console.log('age is 19')
+//   }
+//   else if(age==65){
+//     console.log('age is 65')
+//   }
+//   else{
+//     console.log('age is not 19')
+//   }
+if (doesDrive && age>18){
+  console.log("you can drive");
+}
+else{
+  console.log("you can't drive")
+}
---
+++ object.js
@@ -0,0 +1,15 @@
+
+// const person = {
+//     firstName: 'sehar',
+//     lastName: 'akbar',
+//     age:20,
+//     eyeColor: "blue"
+//   };
+  
+//   console.log(person.firstName.eyeColor);
+const person = {};
+person.firstName = "sehar";
+person.lastName = "akbar";
+person.age = 20;
+person.eyeColor = "blue";
+console.log(person.firstName  + "is" + person.age + "yearsold.");
